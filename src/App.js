00bd212216271dfeca8d82a6191aaa279de8807f
Muradil <04072002mu@gmail.com>
2021-01-27 23:07:40 +0600

    #я сделал homework 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+import bg1 from './assets/bg1.jpg';
+import bg3 from './assets/bg3.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={'This is title'} descr={'This is Description!'}/>
+      <Layout urlBg={bg1} title={'layout 1'} descr={'This is layout 1!'}/>
+      <Layout colorBg="#87AF7C" title={'layout 2'} descr={'This is layout 2!'}/>
+      <Layout urlBg={bg3} title={'layout 3'} descr={'This is layout 3!'}/>
+      <Footer/>
     </div>
   );
 }
